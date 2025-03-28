47491525d6ae4f06f9f2fe42544cdbc3a5ad41e4
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-03-28 17:13:40 -0300

    Use CREATE2 to deploy StylusDeployer
    This change guarantees StylusDeployer will have the same address across
    different networks.

--- run-dev-node.sh
+++ run-dev-node.sh
@@ -2,6 +2,12 @@
 
 NITRO_NODE_VERSION="v3.5.3-0a9c975"  # <-- only update this when you need a new version
 TARGET_IMAGE="offchainlabs/nitro-node:${NITRO_NODE_VERSION}"
+
+RPC=http://127.0.0.1:8547
+PRIVATE_KEY=0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659
+CREATE2_FACTORY=0x4e59b44847b379578588920ca78fbf26c0b4956c
+SALT=0x0000000000000000000000000000000000000000000000000000000000000000
+
 # By default, use nitro docker image. If "--stylus" is passed, build the image with stylus dev dependencies
 STYLUS_MODE="false"
 
@@ -33,12 +39,15 @@ fi
 echo "Starting Nitro dev node..."
 docker run --rm --name nitro-dev -p 8547:8547 "${TARGET_IMAGE}" --dev --http.addr 0.0.0.0 --http.api=net,web3,eth,debug &
 
+# Kill background processes when exiting
+trap 'kill $(jobs -p) 2>/dev/null' EXIT
+
 # Wait for the node to initialize
 echo "Waiting for the Nitro node to initialize..."
 
 until [[ "$(curl -s -X POST -H "Content-Type: application/json" \
   --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}' \
-  http://127.0.0.1:8547)" == *"result"* ]]; do
+  $RPC)" == *"result"* ]]; do
     sleep 0.1
 done
 
@@ -46,7 +55,7 @@ done
 # Check if node is running
 curl_output=$(curl -s -X POST -H "Content-Type: application/json" \
   --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}' \
-  http://127.0.0.1:8547)
+  $RPC)
 
 if [[ "$curl_output" == *"result"* ]]; then
   echo "Nitro node is running!"
@@ -58,13 +67,26 @@ fi
 # Make the caller a chain owner
 echo "Setting chain owner to pre-funded dev account..."
 cast send 0x00000000000000000000000000000000000000FF "becomeChainOwner()" \
-  --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
-  --rpc-url http://127.0.0.1:8547
+  --private-key $PRIVATE_KEY \
+  --rpc-url $RPC
+
+# Set the L1 data fee to 0 so it doesn't impact the L2 Gas limit.
+# This makes the gas estimates closer to Ethereum and allows the deployment of the CREATE2 factory
+cast send -r $RPC --private-key $PRIVATE_KEY 0x0000000000000000000000000000000000000070 'setL1PricePerUnit(uint256)' 0x0
+
+# Deploy CREATE2 factory
+echo "Deploying the CREATE2 factory"
+cast send --rpc-url $RPC --private-key $PRIVATE_KEY --value "1 ether" 0x3fab184622dc19b6109349b94811493bf2a45362
+cast publish --rpc-url $RPC 0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222
+if [ "$(cast code -r $RPC $CREATE2_FACTORY)" == "0x" ]; then
+  echo "Failed to deploy CREATE2 factory"
+  exit 1
+fi
 
 # Deploy Cache Manager Contract
 echo "Deploying Cache Manager contract..."
-deploy_output=$(cast send --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
-  --rpc-url http://127.0.0.1:8547 \
+deploy_output=$(cast send --private-key $PRIVATE_KEY \
+  --rpc-url $RPC \
   --create 0x60a06040523060805234801561001457600080fd5b50608051611d1c61003060003960006105260152611d1c6000f3fe)
 
 # Extract contract address using awk from plain text output
@@ -81,8 +103,8 @@ echo "Cache Manager contract deployed at address: $contract_address"
 
 # Register the deployed Cache Manager contract
 echo "Registering Cache Manager contract as a WASM cache manager..."
-registration_output=$(cast send --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
-  --rpc-url http://127.0.0.1:8547 \
+registration_output=$(cast send --private-key $PRIVATE_KEY \
+  --rpc-url $RPC \
   0x0000000000000000000000000000000000000070 \
   "addWasmCacheManager(address)" "$contract_address")
 
@@ -95,13 +117,12 @@ fi
 echo "Cache Manager deployed and registered successfully"
 
 # Deploy StylusDeployer
-deployer_output=$(cast send --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
-  --rpc-url http://127.0.0.1:8547 \
-  --create $(cat ./stylus-deployer-bytecode.txt))
-deployer_address=$(echo "$deployer_output" | awk '/contractAddress/ {print $2}')
-if [[ -z "$deployer_address" ]]; then
-  echo "Error: Failed to deploy StylusDeployer contract. Full output:"
-  echo "$deployer_output"
+deployer_code=$(cat ./stylus-deployer-bytecode.txt)
+deployer_address=$(cast create2 --salt $SALT --init-code $deployer_code)
+cast send --private-key $PRIVATE_KEY --rpc-url $RPC \
+    $CREATE2_FACTORY "$SALT$deployer_code"
+if [ "$(cast code -r $RPC $deployer_address)" == "0x" ]; then
+  echo "Failed to deploy StylusDeployer"
   exit 1
 fi
 echo "StylusDeployer deployed at address: $deployer_address"
